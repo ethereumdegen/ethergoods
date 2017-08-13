47bbd2be7de47bc48a0b3a640b189077327c356b
andy <andy@jonezie.com>
2017-08-13 17:35:44 -0400

    OMG TESTS DO THINGS

--- test/ethergoods.js
+++ test/ethergoods.js
@@ -3,6 +3,7 @@ var sha3_256 = require('js-sha3').sha3_256;
 
 contract('EtherGoods', function(accounts) {
 
+/*
   it("should register a new good", function() {
     return EtherGoods.deployed().then(function(instance) {
     //  return instance.getBalance.call(accounts[0]);
@@ -14,6 +15,58 @@ contract('EtherGoods', function(accounts) {
       assert.equal(response.initialized, true, "Good is registered");
     });
   });
+  */
+
+  it("can register a good", async function () {
+  var contract = await EtherGoods.deployed();
+
+  var unique_hash = sha3_256("canoeasset");
+  await contract.registerNewGood(accounts[0],unique_hash,"canoe","A wooden boat",5,400);
+
+  assert.equal(contract.goods[unique_hash].initialized, true );
+
+/*
+  var offer = await contract.punksOfferedForSale.call(0);
+  console.log("Offer: " + offer);
+  assert.equal(true, offer[0], "Punk 0 not for sale");
+  assert.equal(0, offer[1]);
+  assert.equal(accounts[0], offer[2]);
+  assert.equal(1000, offer[3]);
+  assert.equal(NULL_ACCOUNT, offer[4]);
+  */
+}),
+
+it("can claim a good", async function () {
+  var contract = await EtherGoods.deployed();
+
+  var unique_hash = sha3_256("canoeasset");
+  await contract.claimGood(unique_hash);
+
+  assert.equal(contract.goods[unique_hash].balanceOf[accounts[0]], 1 );
+
+}),
+
+
+
+/*
+  it("can not buy a punk with an invalid index", async function () {
+      var contract = await CryptoPunksMarket.deployed();
+      await expectThrow(contract.claimGood(100000));
+    }),
+    */
+
+  it("can not get supply while supply all taken", async function () {
+      var contract = await EtherGoods.deployed();
+      var balance = await contract.balanceOf.call(accounts[0]);
+      console.log("Pre Balance: " + balance);
+
+      var allAssigned = await contract.allPunksAssigned.call();
+      console.log("All assigned: " + allAssigned);
+      assert.equal(false, allAssigned, "allAssigned should be false to start.");
+      await expectThrow(contract.getPunk(0));
+      var balance = await contract.balanceOf.call(accounts[0]);
+      console.log("Balance after fail: " + balance);
+    });
 
 /*
   it("should send coin correctly", function() {
